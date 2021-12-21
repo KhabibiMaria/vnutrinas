6bc1de5c0fee91177d820da6c51d75c7b5911d91
danyaqq <fedak220@gmail.com>
2021-12-21 16:41:39 +0300

    add items

---
+++ VnutriNas/View/Menu/DetailItemView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailItemView.swift
+//  VnutriNas
+//
+//  Created by Даня on 21.12.2021.
+//
+
+import SwiftUI
+
+struct DetailItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailItemView()
+    }
+}
